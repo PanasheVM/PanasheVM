58394e0ecf070849b7855aceba23e324d25d107b
panashem <panashem@nmbz.co.zw>
2023-11-20 20:40:17 +0200

    Adding db

--- src/main/java/com/nmbz/servicedev/ServicedevApplication.java
+++ src/main/java/com/nmbz/servicedev/ServicedevApplication.java
@@ -2,8 +2,11 @@ package com.nmbz.servicedev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class ServicedevApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/nmbz/servicedev/controlllers/ResourceController.java
+++ src/main/java/com/nmbz/servicedev/controlllers/ResourceController.java
@@ -1,4 +1,19 @@
 package com.nmbz.servicedev.controlllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ResourceController {
+    @GetMapping("/home")
+    public String homeEndpoint() {
+        return "Account Operations";
+    }
+
+    @GetMapping("/accountfreeze/")
+    public String accountFreeze() {
+        return "Account Freeze";
+    }
+
 }
+
