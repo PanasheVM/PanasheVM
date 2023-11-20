8cfa456cfa188a31d86bfcc6dec95601639add5c
panashem <panashem@nmbz.co.zw>
2023-11-20 20:54:56 +0200

    Adding ssh connection to server

--- src/main/java/com/nmbz/servicedev/ServicedevApplication.java
+++ src/main/java/com/nmbz/servicedev/ServicedevApplication.java
@@ -1,5 +1,6 @@
 package com.nmbz.servicedev;
 
+import com.nmbz.servicedev.components.ConnectToServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -11,6 +12,14 @@ public class ServicedevApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServicedevApplication.class, args);
+        String user = "panashem";
+        String host = "127.0.0.1";
+        int port = 22;
+        String password = "Pvm@8941";
+        String remoteFilePath = "/home/panashem/";
+
+        ConnectToServer connectToServer = new ConnectToServer(user, host, port, password, remoteFilePath);
+        connectToServer.startMonitoring();
     }
 
 }
